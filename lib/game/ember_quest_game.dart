c620c013f84dd5adbfd22a37f7af57c9e1d798be
keybow <keybow007@gmail.com>
2024-06-06 22:00:43 +0900

    20240606:4. Building the World途中まで

--- lib/game/ember_quest_game.dart
+++ lib/game/ember_quest_game.dart
@@ -1,4 +1,9 @@
 import 'package:ember_quest/game/actors/ember.dart';
+import 'package:ember_quest/game/actors/water_enemy.dart';
+import 'package:ember_quest/game/managers/segment_manager.dart';
+import 'package:ember_quest/game/objects/ground_block.dart';
+import 'package:ember_quest/game/objects/platform_block.dart';
+import 'package:ember_quest/game/objects/star.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
@@ -21,10 +26,35 @@ class EmberQuestGame extends FlameGame {
 
     //画面の原点座標を左上に設定
     camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  void initializeGame() {
+
+    //TODo 仮
+    loadGameSegments(0);
 
     _emberPlayer = EmberPlayer(
       position: Vector2(128, canvasSize.y - 70),
     );
     world.add(_emberPlayer);
+  }
+
+  void loadGameSegments(int segmentIndex) {
+    final blocks = segments[segmentIndex];
+    for (int i = 0; i < blocks.length; i++) {
+      final block = blocks[i];
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+        case PlatformBlock:
+          add(PlatformBlock(gridPosition: block.gridPosition));
+          break;
+        case Star:
+          break;
+        case WaterEnemy:
+          break;
+      }
+    }
   }
 }
--- lib/game/objects/platform_block.dart
+++ lib/game/objects/platform_block.dart
@@ -1,5 +1,26 @@
+import 'package:ember_quest/game/ember_quest_game.dart';
 import 'package:flame/components.dart';
 
-class PlatformBlock extends Component {
+class PlatformBlock extends SpriteComponent
+    with HasGameReference<EmberQuestGame> {
+  final Vector2 gridPosition;
+
   //TODO
+  PlatformBlock({required this.gridPosition})
+      : super(
+          size: Vector2.all(64),
+          anchor: Anchor.bottomLeft,
+        );
+
+  @override
+  void onLoad() {
+    super.onLoad();
+    final image = game.images.fromCache("block.png");
+    sprite = Sprite(image);
+
+    position = Vector2(
+      size.x * gridPosition.x,
+      game.size.y - size.y * gridPosition.y,
+    );
+  }
 }
